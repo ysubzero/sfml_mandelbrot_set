08e191f7f8b20a2c84df3224b178f2c757083706
fraudulizer <eljandy42@gmail.com>
2025-01-12 03:10:56 +0800

    Add project files.

---
+++ src/headers/header.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+#include <iostream>
+#include <ostream>
+#include <memory>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <random>
+#include <algorithm>
+#include <thread>
+#include <cstdint>
+#include <cstdlib>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <queue>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/System/Vector2.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+struct Complex
+{
+	double real;
+	double imag;
+
+	Complex(double _real = 0, double _imag = 0) :
+		real(_real),
+		imag(_imag)
+	{
+
+	}
+
+	Complex(const Complex& other):
+		real(other.real),
+		imag(other.imag)
+	{
+
+	}
+
+	Complex& operator=(const Complex& other)
+	{
+		if (this != &other)
+		{
+			real = other.real;
+			imag = other.imag;
+		}
+
+		return *this;
+	}
+
+	Complex operator+(const Complex& other)
+	{
+		return Complex(real + other.real, imag + other.imag);
+	}
+
+	Complex operator+(const double& realNum) {
+		return Complex(real + realNum, imag);
+	}
+	
+	Complex operator-(const Complex& other)
+	{
+		return Complex(real - other.real, imag - other.imag);
+	}
+
+	Complex operator*(const Complex& other)
+	{
+		return Complex(real * other.real - imag * other.imag, real * other.imag + other.real * imag);
+	}
+
+	Complex operator/(const Complex& other)
+	{
+		double denom = other.real * other.real + other.imag * other.imag;
+		if (denom == 0)
+		{
+			std::cerr << "Error: Division by zero!" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		double returnreal = (real * other.real + imag * other.imag) /denom;
+		double returnimag = (imag * other.real - real * other.imag) /denom;
+
+		return Complex(returnreal, returnimag);
+	}
+
+	bool operator==(const Complex& other)
+	{
+		return (real == other.real) && (imag == other.imag);
+	}
+
+	double magnitude()
+	{
+		return std::sqrt(real * real + imag * imag);
+	}
+
+	double sqr_magnitude()
+	{
+		return (real * real + imag * imag);
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Complex& c);
+};
+
+Complex square(const Complex& c)
+{
+	return Complex(c.real * c.real - c.imag * c.imag, 2 * c.real * c.imag);
+}
+
+std::ostream& operator<<(std::ostream& os, const Complex& c)
+{
+	os << c.real << "+" << c.imag << "i";
+	return os;
+}
---
+++ src/main.cpp
@@ -0,0 +1,440 @@
+#include "headers/header.hpp"
+
+bool usegoodcolor = true;
+
+class mandelbrot
+{
+private:
+	int numX;
+	int numY;
+	int num;
+
+	int topbound = 2;
+	int startbound = -2;
+	double bound = 4;
+	int maxiter = 1024;
+
+	std::vector<Complex> set;
+	std::vector<int> inset;
+
+	std::mutex mtx;
+
+public:
+
+	std::vector<uint8_t> bitmap;
+
+	std::atomic<uint32_t> done = 0;
+	
+	mandelbrot(int X, int Y):
+		numX(X),
+		numY(Y)
+	{
+		num = numX * numY;
+		set.resize(num);
+		inset.resize(num, 1);
+	}
+
+	void threaded_emplacement(const int& start, const int& end)
+	{
+		for (int i = start; i < end; i++)
+		{
+			Complex c = set[i];
+			Complex z = Complex(0, 0);
+			bool diverge = false;
+			for (int iter = 1; iter < maxiter; iter++)
+			{
+				if (z.sqr_magnitude() >= bound)
+				{
+					diverge = true;
+					inset[i] = iter;
+					break;
+				}
+				z = z * z + c;
+			}
+			if (!diverge)
+			{
+				std::lock_guard<std::mutex> lock(mtx);
+				inset[i] = maxiter;
+			}
+			done++;
+		}
+		std::cout << "Thread done.\n";
+		while (done > set.size())
+		{
+			std::this_thread::yield();
+		}
+	}
+
+	void calculate(std::vector<std::thread>& threads)
+	{
+		for (int i = 0; i < numX; i++)
+		{
+			for (int j = 0; j < numY; j++)
+			{
+				int index = i * numY + j;
+				double imag = startbound + (j * ((topbound - startbound) / static_cast<double>(numY)));
+				double real = (i * ((topbound - startbound) / static_cast<double>(numX))) - topbound;
+				set[index] = Complex(real, imag);
+			}
+		}
+
+		std::cout << "done complex mapping \n";
+
+		for (int i = 0; i < threads.size(); i++)
+		{
+			int start = i * (set.size() / threads.size());
+			int end = (i+1) * (set.size() / threads.size());
+			if (i + 1 == threads.size())
+			{
+				end = set.size();
+			}
+
+			threads[i] = std::thread(&mandelbrot::threaded_emplacement, this, start, end);
+		}
+
+		for (auto& t : threads)
+		{
+			if (t.joinable())
+			{
+				t.join();
+			}
+		}
+
+		threads.erase(threads.begin(), threads.end());
+
+		std::cout << "done mandelbrot calculating \n";
+
+	}
+
+	void draw(sf::Image& image, std::string& filename)
+	{
+		uint16_t  type = 0x4D42;
+		uint16_t  reserved1 = 0;
+		uint16_t  reserved2 = 0;
+		uint32_t  offset = 54;
+		uint32_t  dib_header_size = 40;
+		int32_t   width_px = numX;
+		int32_t   height_px = numY;
+		uint16_t  num_planes = 1;
+		uint16_t  bits_per_pixel = 24;
+		uint32_t  compression = 0;
+		uint32_t  image_size_bytes = numX * numY * 3;
+		uint32_t  size = image_size_bytes + 54;
+
+		float image_aspect_ratio = static_cast<float>(numX) /
+			static_cast<float>(numY);
+
+		float print_width_meters = 0.2f;
+
+		float print_height_meters = print_width_meters / image_aspect_ratio;
+
+		int32_t x_resolution_ppm = static_cast<int32_t>(numX / print_width_meters);
+		int32_t y_resolution_ppm = static_cast<int32_t>(numX / print_height_meters);
+
+		uint32_t  num_colors = 0;
+		uint32_t  important_colors = 0;
+
+		bitmap.push_back((uint8_t)(type & 0xFF));
+		bitmap.push_back((uint8_t)((type >> 8) & 0xFF));
+
+		bitmap.push_back((uint8_t)(size & 0xFF));
+		bitmap.push_back((uint8_t)((size >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)((size >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)((size >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)(reserved1 & 0xFF));
+		bitmap.push_back((uint8_t)((reserved1 >> 8) & 0xFF));
+
+		bitmap.push_back((uint8_t)(reserved2 & 0xFF));
+		bitmap.push_back((uint8_t)((reserved2 >> 8) & 0xFF));
+
+		bitmap.push_back((uint8_t)(offset & 0xFF));
+		bitmap.push_back((uint8_t)((offset >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)((offset >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)((offset >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)(dib_header_size & 0xFF));
+		bitmap.push_back((uint8_t)((dib_header_size >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)((dib_header_size >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)((dib_header_size >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)((width_px) & 0xFF));
+		bitmap.push_back((uint8_t)(((width_px) >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)(((width_px) >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)(((width_px) >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)((height_px) & 0xFF));
+		bitmap.push_back((uint8_t)(((height_px) >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)(((height_px) >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)(((height_px) >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)(num_planes & 0xFF));
+		bitmap.push_back((uint8_t)((num_planes >> 8) & 0xFF));
+
+		bitmap.push_back((uint8_t)(bits_per_pixel & 0xFF));
+		bitmap.push_back((uint8_t)((bits_per_pixel >> 8) & 0xFF));
+
+		bitmap.push_back((uint8_t)(compression & 0xFF));
+		bitmap.push_back((uint8_t)((compression >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)((compression >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)((compression >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)(image_size_bytes & 0xFF));
+		bitmap.push_back((uint8_t)((image_size_bytes >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)((image_size_bytes >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)((image_size_bytes >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)((x_resolution_ppm) & 0xFF));
+		bitmap.push_back((uint8_t)(((x_resolution_ppm) >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)(((x_resolution_ppm) >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)(((x_resolution_ppm) >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)((y_resolution_ppm) & 0xFF));
+		bitmap.push_back((uint8_t)(((y_resolution_ppm) >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)(((y_resolution_ppm) >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)(((y_resolution_ppm) >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)(num_colors & 0xFF));
+		bitmap.push_back((uint8_t)((num_colors >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)((num_colors >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)((num_colors >> 24) & 0xFF));
+
+		bitmap.push_back((uint8_t)(important_colors & 0xFF));
+		bitmap.push_back((uint8_t)((important_colors >> 8) & 0xFF));
+		bitmap.push_back((uint8_t)((important_colors >> 16) & 0xFF));
+		bitmap.push_back((uint8_t)((important_colors >> 24) & 0xFF));
+
+		sf::Color mapping[16];
+		mapping[0] = sf::Color(70, 30, 10, 255);
+		mapping[1] = sf::Color(25, 5, 25, 255);
+		mapping[2] = sf::Color(10, 0, 50, 255);
+		mapping[3] = sf::Color(5, 5, 75, 255);
+		mapping[4] = sf::Color(0, 5, 100, 255);
+		mapping[5] = sf::Color(15, 45, 140, 255);
+		mapping[6] = sf::Color(25, 85, 180, 255);
+		mapping[7] = sf::Color(60, 125, 210, 255);
+		mapping[8] = sf::Color(135, 185, 230, 255);
+		mapping[9] = sf::Color(215, 240, 250, 255);
+		mapping[10] = sf::Color(245, 235, 195, 255);
+		mapping[11] = sf::Color(250, 200, 95, 255);
+		mapping[12] = sf::Color(255, 170, 0, 255);
+		mapping[13] = sf::Color(205, 130, 0, 255);
+		mapping[14] = sf::Color(155, 90, 0, 255);
+		mapping[15] = sf::Color(105, 5, 5, 255);
+
+		for (int j = numY - 1; j >= 0; j--)
+		{
+			for (int i = 0; i < numX; i++)
+			{
+
+				int idx = i * numY + j;
+				
+				if (usegoodcolor)
+				{
+					sf::Color color;
+
+					int n = inset[idx];
+					if (n < maxiter && n > 0) {
+						int i = n % 16;
+						color = mapping[i];
+					}
+					else
+					{
+						color = sf::Color::Black;
+					}
+					uint8_t r = color.r;
+					uint8_t g = color.g;
+					uint8_t b = color.b;
+
+
+					image.setPixel(i, j, color);
+
+
+					bitmap.push_back(b);
+					bitmap.push_back(g);
+					bitmap.push_back(r);
+				}
+				else
+				{
+					double map360 = (inset[idx] / static_cast<double>(maxiter)) * 360.0;
+
+					int h = static_cast<int>(std::pow(map360, 1.5)) % 360;
+					int s = 1;
+					int v = (inset[idx] / maxiter);
+
+					double c = v * s;
+					int x = c * (1 - std::abs(((h / 60) % 2) - 1));
+					double m = v - c;
+
+					double rt;
+					double gt;
+					double bt;
+
+					if (h >= 300)
+					{
+						rt = c;
+						bt = 0;
+						gt = x;
+					}
+					else if (h >= 240)
+					{
+						rt = 0;
+						bt = c;
+						gt = x;
+					}
+					else if (h >= 180)
+					{
+						rt = 0;
+						bt = x;
+						gt = c;
+					}
+					else if (h >= 120)
+					{
+						rt = 0;
+						bt = c;
+						gt = x;
+					}
+					else if (h >= 60)
+					{
+						rt = x;
+						bt = c;
+						gt = 0;
+					}
+					else
+					{
+						rt = c;
+						bt = x;
+						gt = 0;
+					}
+
+					uint8_t r = (rt + m) * 255;
+					uint8_t g = (gt + m) * 255;
+					uint8_t b = (bt + m) * 255;
+
+					sf::Color color = sf::Color(r, g, b, 255);
+					image.setPixel(i, j, color);
+
+					bitmap.push_back(b);
+					bitmap.push_back(g);
+					bitmap.push_back(r);
+				}
+
+			}
+		}
+
+		std::ofstream outputFile(filename, std::ios::binary);
+
+		if (outputFile.is_open()) {
+			outputFile.write(reinterpret_cast<const char*>(bitmap.data()), bitmap.size());
+			outputFile.close();
+		}
+		else 
+		{
+			std::cerr << "Error: Could not open output file." << std::endl;
+		}
+
+		std::cout << "done drawing \n";
+
+		set.erase(set.begin(), set.end());
+		inset.erase(inset.begin(), inset.end());
+		bitmap.erase(bitmap.begin(), bitmap.end());
+	}
+};
+
+void checkinput(sf::RenderWindow& window, float& zoomFactor, int& deltaY, int& deltaX, sf::View& view, int& resX, int& resY)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		zoomFactor += 0.02;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && zoomFactor > 0.01f) {
+		zoomFactor -= 0.02;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+		deltaY -= 10 * zoomFactor;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+		deltaY += 10 * zoomFactor;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		deltaX -= 10 * zoomFactor;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		deltaX += 10 * zoomFactor;
+	}
+	view.setSize(resX * zoomFactor, resY * zoomFactor);
+	view.setCenter(resX / 2 + deltaX, resY / 2 + deltaY);
+}
+
+int main()
+{
+	std::string filename;
+	if (usegoodcolor)
+	{
+		filename = "D:\\source\\mandelbrot_set\\mandelbrot.bmp";
+	}
+	else
+	{
+		filename = "D:\\source\\mandelbrot_set\\mandelbrot_basic.bmp";
+	}
+
+	uint32_t max_threads = std::thread::hardware_concurrency();
+	std::vector<std::thread> threads;
+	threads.resize(max_threads);
+	int resX = 1200;
+	int resY = 1200;
+
+	int mandelbrotX = 12000;
+	int mandelbrotY = 12000;
+
+	sf::Image image;
+	image.create(mandelbrotX, mandelbrotY, sf::Color::Black);
+	mandelbrot mb(mandelbrotX, mandelbrotY);
+	mb.calculate(threads);
+	mb.draw(image, filename);
+
+	sf::RenderWindow window = sf::RenderWindow({ static_cast<uint32_t>(resX), static_cast<uint32_t>(resY) }, "Mandlebrot", sf::Style::Titlebar | sf::Style::Close);
+	sf::View view(sf::FloatRect(0, 0, resX, resY));
+	view.setCenter(resX / 2, resY / 2);
+	window.setView(view);
+
+	float zoomFactor = 1;
+	int deltaY = 0;
+	int deltaX = 0;
+
+	window.setFramerateLimit(170);
+
+	sf::Texture texture;
+	texture.loadFromImage(image);
+
+	sf::Sprite sprite(texture);
+
+
+	while (window.isOpen())
+	{
+		window.clear(sf::Color::White);
+
+		for (auto event = sf::Event(); window.pollEvent(event);)
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::R))
+			{
+				view.setCenter(resX / 2, resY / 2);
+				zoomFactor = 1;
+				deltaY = 0;
+				deltaX = 0;
+			}
+		}
+
+		checkinput(window, zoomFactor, deltaY, deltaX, view, resX, resY);
+		window.setView(view);
+
+		window.draw(sprite);
+		window.display();
+
+	}
+	return 0;
+}
